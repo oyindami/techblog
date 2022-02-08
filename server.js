6ec935d965884fa1f03eaf9b1920cc842b7b4f66
Oyindamola Peterson <oyindamolapeterson@Oyindamolas-MacBook-Air.local>
2022-02-08 10:37:06 -0500

    adding all to all files

--- server.js
+++ server.js
@@ -1,48 +1,41 @@
-const express = require("express");
-const route = require("./controllers");
-const sequelize = require("./config/connection");
-const path = require("path");
-const exphbs = require("express-handlebars");
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const session = require("express-session");
-const helpers = require("./utils/helpers");
-const hbs = exphbs.create({ helpers });
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-// Session is setup so that it expires after 30 secs of no activity on the page. It's short only for testing purposes.
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
 const sess = {
-  secret: "Super secret secret",
-  cookie: { maxAge: 30 * 1000 },
-  resave: true,
-  rolling: true,
-  saveUninitialized: false,
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize,
-  }),
+    db: sequelize
+  })
 };
 
-const hsb = exphbs.create({
-  helpers: {
-    format_date: (date) => {
-      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    },
-  },
-});
-
 app.use(session(sess));
-app.use(express.static(path.join(__dirname, "public")));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.engine("handlebars", hsb.engine);
-app.set("view engine", "handlebars");
+app.use(routes);
 
-// to initilize the routes
-app.use(route);
-
-// to initilize the server adn the db
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("App is now listening"));
+  app.listen(PORT, () => console.log('Now listening'));
 });
